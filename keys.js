27351f367e288ce0c5f2e8cbdf8602eca99f0fc4
Philip <nogobaduk@gmail.com>
2017-09-29 01:05:04 -0700

    edits required

---
+++ keys.js
@@ -0,0 +1,12 @@
+var twitterKeys = {
+
+  consumer_key: "cRp5FeYUT53c1NG66oNcyxQOF",
+  onsumer_secret: "NvMOuEhERCKhj5OEvACEgKhlYu2kqunuxZEgew6cBIeBVTynIO",
+  access_token_key: "912733824580124672-h7EzKy5XssDtooeAWTM8bTYYeZdrkMl",
+  access_token_secret: "BU5hBJwQLNwi5QjbhNnzJXKAJ2FW52g7MPsF5UnpqDiFG",
+};
+
+module.exports =  { 
+
+	twitterKeys: twitterKeys
+}
